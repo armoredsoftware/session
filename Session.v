6b3eaa3ec01b18e9dcaff2740259c931be1d8cda
Adam Petz <apetz@g.emporia.edu>
2015-11-22 15:22:51 -0600

    Initial attempt at base types

---
+++ Session.v
@@ -0,0 +1,85 @@
+Require Export SfLib.
+
+Module try1.
+  
+Inductive ProtoA : Type :=
+| SendA : forall (A:Type), ProtoA -> ProtoA
+| RecA : forall (A:Type), ProtoA -> ProtoA
+| ChoiceA : ProtoA -> ProtoA -> ProtoA
+| OfferA : ProtoA -> ProtoA -> ProtoA                                
+| Eps : ProtoA.                                 
+
+Notation "x :!: y" := (SendA x y)
+                          (at level 50, left associativity).
+
+Notation "x :?: y" := (RecA x y)
+                        (at level 50, left associativity).
+
+Notation "x :+: y" := (ChoiceA x y)
+                        (at level 50, left associativity).
+
+Notation "x :&: y" := (OfferA x y)
+                          (at level 50, left associativity).
+
+Check (nat :!: (bool :?: Eps)).
+
+Inductive Dual : ProtoA -> ProtoA -> Prop :=
+| senRec : forall (A:Type) (R S : ProtoA),
+    Dual R S -> Dual (A :!: R) (A :?: S)
+| recSen : forall (A:Type) (R S : ProtoA),
+    Dual R S -> Dual (A :?: R) (A :!: S)                    
+| choiceOffer : forall (R S T U: ProtoA),
+    Dual R S -> Dual T U -> Dual (R :+: T) (S :&: U)
+| offerChoice : forall (R S T U: ProtoA),
+    Dual R S -> Dual T U -> Dual (R :&: T) (S :+: U)                            
+| epsDual : Dual Eps Eps.
+
+Example dual1 : Dual (nat :!: Eps) (nat :?: Eps).
+Proof.
+  apply senRec.
+  apply epsDual.
+Qed.
+
+Hint Constructors Dual.
+
+Example dual2 : Dual (nat :!: (bool :?: Eps)) (nat :?: (bool :!: Eps)).
+Proof. auto. Qed.
+
+Example dual3 : Dual ((nat :!: (bool :?: Eps)) :+: (bool :!: (nat :?: Eps)))
+                     ((nat :?: (bool :!: Eps)) :&: (bool :?: (nat :!: Eps))).
+Proof. auto. Qed.
+
+(* How can we make the type of send more expressive?  It should be something like (a :!: r) -> r *)
+Inductive Session (P P':ProtoA) (T:Type) :=
+| send {A:Type} :  A -> Session P P' T.
+                                     
+
+Check (Session (nat :!: Eps) (nat :?: Eps) nat).
+
+End try1.
+
+
+Module try2.
+
+Inductive sendT (A:Type) (R:Type) :=.
+Inductive recieveT (A:Type) (R:Type) :=.
+Inductive choiceT (R:Type) (S:Type) :=.
+Inductive offerT (R:Type) (S:Type) :=.
+
+Notation "x :!: y" := (sendT x y)
+                          (at level 50, left associativity).
+
+Notation "x :?: y" := (recieveT x y)
+                        (at level 50, left associativity).
+
+Notation "x :+: y" := (choiceT x y)
+                        (at level 50, left associativity).
+
+Notation "x :&: y" := (offerT x y)
+                        (at level 50, left associativity).
+
+(*Definition send {A:Type} {R:Type} : (A :!: R) -> A -> R := *)
+
+End try2.
+
+  
